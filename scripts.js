953c5359c6b31b0bfed3d0551426c7b7549ddb7a
atvars <avelavs@gmail.com>
2025-07-30 13:57:02 +0000

    checking if data is returned

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1c0234328d5c27cddcafaba64af270df&pag=1'
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=1c0234328d5c27cddcafaba64af270df&query="'
+
+
+// get initial movies
+getMovies(API_URL)
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data.results)
+}
+
